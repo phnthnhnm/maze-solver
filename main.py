23abea1e87856a533a4517bd251ba71bb4bbd641
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-08 18:27:06 +0700

    feat: add points and lines

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from graphics import Window
+from graphics import Window, Line, Point
+
 
 def main():
-    win = Window(800, 600)
-    win.wait_for_close()
+    window = Window(800, 600)
+    line = Line(Point(50, 50), Point(400, 400))
+    
+    window.draw_line(line, "red")
+    window.wait_for_close()
 
-if __name__ == "__main__":
-    main()
+
+main()
