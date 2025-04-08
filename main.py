f60d9b8c5706939962bf1ddefbed3da864d05e9f
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-08 18:40:51 +0700

    feat: add mazes

--- main.py
+++ main.py
@@ -1,33 +1,18 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 
 def main():
-    window = Window(800, 600)
+    num_rows = 12
+    num_columns = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_columns
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    window = Window(screen_x, screen_y)
 
-    c1 = Cell(window)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
-
-    c2 = Cell(window)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-
-    c1.draw_move(c2)
-
-    c3 = Cell(window)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-
-    c2.draw_move(c3)
-
-    c4 = Cell(window)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-
-    c3.draw_move(c4, True)
+    maze = Maze(margin, margin, num_rows, num_columns, cell_size_x, cell_size_y, window)
 
     window.wait_for_close()
 
