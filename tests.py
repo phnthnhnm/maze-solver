74d1bee69610156dc709ef1ac1623c4c1984a080
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-08 18:47:06 +0700

    feat: remove entrance and exit walls

--- tests.py
+++ tests.py
@@ -30,6 +30,19 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_break_entrance_and_exit(self):
+        num_columns = 12
+        num_rows = 10
+        maze = Maze(0, 0, num_rows, num_columns, 10, 10)
+        self.assertEqual(
+            maze._cells[0][0].has_top_wall,
+            False,
+        )
+        self.assertEqual(
+            maze._cells[num_columns - 1][num_rows - 1].has_bottom_wall,
+            False,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
