04ee469c7ae64e4049c1d07ca7df943cf65862c4
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-08 18:35:48 +0700

    feat: add a way to draw a path between 2 cells

--- main.py
+++ main.py
@@ -5,21 +5,29 @@ from cell import Cell
 def main():
     window = Window(800, 600)
 
-    cell = Cell(window)
-    cell.has_left_wall = False
-    cell.draw(50, 50, 100, 100)
+    c1 = Cell(window)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    cell = Cell(window)
-    cell.has_right_wall = False
-    cell.draw(125, 125, 200, 200)
+    c2 = Cell(window)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    cell = Cell(window)
-    cell.has_bottom_wall = False
-    cell.draw(225, 225, 250, 250)
+    c1.draw_move(c2)
 
-    cell = Cell(window)
-    cell.has_top_wall = False
-    cell.draw(300, 300, 500, 500)
+    c3 = Cell(window)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(window)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
 
     window.wait_for_close()
 
