e5b1576239e007ed51bd44174858c0e0b8e21b39
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-04-08 18:55:03 +0700

    feat: add a way to reset a cell's 'visited' state

--- maze.py
+++ maze.py
@@ -30,7 +30,8 @@ class Maze:
         self._create_cells()
         self._break_entrance_and_exit()
         self._break_walls_r(0, 0)
-
+        self._reset_cells_visited()
+        
     def _create_cells(self):
         for i in range(self._num_columns):
             col_cells = []
@@ -103,3 +104,8 @@ class Maze:
                 self._cells[i][j - 1].has_bottom_wall = False
 
             self._break_walls_r(next_index[0], next_index[1])
+
+    def _reset_cells_visited(self):
+        for column in self._cells:
+            for cell in column:
+                cell.visited = False
